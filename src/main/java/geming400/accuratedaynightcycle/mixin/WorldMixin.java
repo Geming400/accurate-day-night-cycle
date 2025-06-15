40fe33f80e646b45c33dcbde5fad57513bcb76da
Geming400 <Geming400@gmail.com>
2025-06-15 18:15:59 +0200

    Implemented accurate day night cycle

--- src/main/java/geming400/accuratedaynightcycle/mixin/WorldMixin.java
+++ src/main/java/geming400/accuratedaynightcycle/mixin/WorldMixin.java
@@ -1,58 +1,72 @@
 package geming400.accuratedaynightcycle.mixin;
 
+import com.mojang.serialization.Codec;
 import geming400.accuratedaynightcycle.AccurateDaynightCycle;
 import net.minecraft.world.World;
-import net.minecraft.world.dimension.DimensionType;
-import org.shredzone.commons.suncalc.MoonIllumination;
-import org.shredzone.commons.suncalc.SunTimes;
+import org.shredzone.commons.suncalc.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Objects;
 
+import static geming400.accuratedaynightcycle.moonphases.MoonPhases.getMCmoonPhaseOffset;
+
 @Mixin(World.class)
 public abstract class WorldMixin {
-    /*
-        a mc day ∈ [0, 1200] ticks
-    */
     @Unique
-    final double mcDayLenghtFactor = (double) 180 / 432;
+    private int iter = 0;
 
     @Unique
-    int iter = 0;
+    private long lastTime = 0;
 
     @Unique
-    long lastTime = 0;
-
-    @Unique
-    private int localTimeToSecond(LocalTime time) {
+    private int localTimeToSeconds(LocalTime time) {
         return time.getHour() * 60 * 60
-                + time.getMinute() * 60
-                + time.getSecond();
+                        + time.getMinute() * 60
+                        + time.getSecond();
+    }
+    @Unique
+    private int localTimeToSeconds(ZonedDateTime time) {
+        return localTimeToSeconds(time.toLocalTime());
     }
 
     @Unique
-    private long timeToTick(LocalTime time) {
-        /*
-        localTimeToSecond(time) ∈ [0, 86400]
-         */
-        return (long) -(localTimeToSecond(time) * mcDayLenghtFactor);
+    private long timeToMcTime(LocalTime time) {
+        // This is the simplified formula
+        // Original one that I found:
+        // 24000 - ((localTimeToSeconds(time) / 86400) * 24000 + 6000
+        return timeToMcTime(localTimeToSeconds(time));
+    }
+    @Unique
+    private long timeToMcTime(ZonedDateTime time) {
+        // This is the simplified formula
+        // Original one that I found:
+        // 24000 - ((localTimeToSeconds(time) / 86400) * 24000 + 6000
+        return timeToMcTime(localTimeToSeconds(time));
+    }
+    @Unique
+    private long timeToMcTime(long time) {
+        // This is the simplified formula
+        // Original one that I found:
+        // 24000 - ((time / 86400) * 24000 + 6000
+        return -(5L * time) / 18  +  18000;
     }
 
     @Unique
     private boolean isTimeZoneSet() {
-        return Objects.equals(AccurateDaynightCycle.CONFIG.gmtTimeZone(), "") || Objects.equals(AccurateDaynightCycle.CONFIG.gmtTimeZone(), "auto");
+        return !(Objects.equals(AccurateDaynightCycle.CONFIG.gmtTimeZone(), "") || Objects.equals(AccurateDaynightCycle.CONFIG.gmtTimeZone(), "auto"));
     }
 
     /**
      * @author geming400
      * @reason need to change the returned value
      */
+    @SuppressWarnings("null")
     @Overwrite
     public long getTimeOfDay() {
         if (iter < AccurateDaynightCycle.CONFIG.calculateEachXticks()) {
@@ -62,35 +76,42 @@ public abstract class WorldMixin {
 
         iter = 0;
 
-        AccurateDaynightCycle.LOGGER.debug("Calculating sun pos");
+        ZoneId zoneId;
+        ZonedDateTime zonedDateTime;
+
+        if (isTimeZoneSet()) {
+            zoneId = ZoneOffset.of(AccurateDaynightCycle.CONFIG.gmtTimeZone());
+        } else {
+            zoneId = ZoneOffset.systemDefault();
+        }
+
+        zonedDateTime = ZonedDateTime.now(zoneId);
+
+        long MCmoonOffset = getMCmoonPhaseOffset(zonedDateTime);
 
         if (AccurateDaynightCycle.CONFIG.useTimeLibrary()) {
-            LocalTime localTime;
-            if (isTimeZoneSet()) {
-                localTime = LocalTime.now();
-            } else {
-                localTime = LocalTime.now(ZoneOffset.of(AccurateDaynightCycle.CONFIG.gmtTimeZone()));
-            }
-
-            lastTime = timeToTick(localTime);
-
-            return lastTime;
+            lastTime = timeToMcTime(zonedDateTime);
+            return lastTime + MCmoonOffset;
         } else {
-            ZonedDateTime zonedDateTime;
-
-            if (isTimeZoneSet()) {
-                zonedDateTime = ZonedDateTime.now();
-            } else {
-                zonedDateTime = ZonedDateTime.now(ZoneOffset.of(AccurateDaynightCycle.CONFIG.gmtTimeZone()));
-            }
-
             SunTimes sunTimes = SunTimes.compute()
                     .on(zonedDateTime)
+                    .timezone(zoneId)
                     .at(AccurateDaynightCycle.CONFIG.latitude(), AccurateDaynightCycle.CONFIG.longitude())
                     .execute();
 
+            long mcTime = lastTime;
 
+            if (sunTimes.getNoon() != null) {
+                // 43200 = 24H/2
+                double timeFactor = (double) localTimeToSeconds(zonedDateTime) / localTimeToSeconds(sunTimes.getNoon());
+                mcTime = timeToMcTime(
+                        (long) (timeFactor * 43200L)
+                );
 
+                //AccurateDaynightCycle.LOGGER.info(String.valueOf(mcTime));
+            }
+
+            lastTime = (mcTime + MCmoonOffset);
             return lastTime;
         }
     }
